86c5fe2ee7fcb5c6ed9b670b41abf29d527d8b5c
Lê Quốc Trung <115451384+Danny2611@users.noreply.github.com>
2025-05-17 00:11:41 +0700

    config route for auth

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Routes from "./routes/index";
 import { BrowserRouter } from "react-router-dom";
-
+import { AuthProviderWithRouter } from "./contexts/AuthContext";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <BrowserRouter>
-
+        <AuthProviderWithRouter>
           <Routes />
-      
+        </AuthProviderWithRouter>
       </BrowserRouter>
     </div>
   );
--- fe/src/routes/publicRoutes.tsx
+++ fe/src/routes/publicRoutes.tsx
@@ -1,9 +1,18 @@
+import Login from "~/pages/auth/Login";
 import Home from "../pages/public/Home/Home";
+import Register from "~/pages/auth/Register";
+import ForgotPassword from "~/pages/auth/ForgotPassword";
+import VerifyOTP from "~/pages/auth/VerifyOTP";
+import OAuthCallback from "~/pages/auth/OAuthCallback";
 
 
 const publicRoutes = [
   { path: "/", element: <Home /> },
-  
+   { path: "login", element: <Login /> },
+  { path: "register", element: <Register /> },
+  { path: "forgot-password", element: <ForgotPassword /> },
+  { path: "verify-otp", element: <VerifyOTP /> },
+  { path: "oauth/callback", element: <OAuthCallback /> },
 ];
 
 export default publicRoutes;
