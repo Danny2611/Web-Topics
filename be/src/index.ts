3c5f216d2c6bb194f97f44918acdbc3318217c25
nicha2301 <nqt230103a@gmail.com>
2025-05-22 23:53:45 +0700

    add rout confg package.json

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,29 +1,49 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+// export default app;
 
-dotenv.config();
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import passport from 'passport';
+
+
+// config
+import connectDB  from './config/db';
+import corsConfig from './config/cors';
+import './config/passport'; // Import cấu hình OAuth
+import cookieParser from 'cookie-parser'; // Đọc và xử lý cookie từ request
+
+
+//middleware
+import {errorHandler} from './middlewares/errorHandler';
+
+// routes
+import authRoutes from './routes/api/authRoutes';
+// import userRoutes from "./routes/api/userRoutes";
+// import adminRoutes from '~/routes/api/adminRoutes';
+import publicRoutes from "./routes/api/publicRoutes";
+
+
+
+dotenv.config(); // Load biến môi trường từ file .env
+const PORT = process.env.PORT ; // Lấy PORT từ .env, nếu không có thì dùng 5000
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+connectDB();
+
 app.use(express.json());
+app.use(passport.initialize());
+app.use(cors(corsConfig.current));
+app.use(errorHandler);
+app.use(cookieParser());
 
-// MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gym-managements';
-mongoose.connect(MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Gym Management System API');
-});
-
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/public', express.static('public'));
+app.use('/api/auth', authRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/admin", adminRoutes);
+app.use("/api/public", publicRoutes);
+export default app;
