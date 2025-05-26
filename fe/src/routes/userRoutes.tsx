135eabc2d8416d956f012e7f8f1d299d9c1ee70f
Danny2611 <21130584@st.hcmuaf.edu.vn>
2025-05-26 23:59:23 +0700

    changepassword, package, packagedetail

--- fe/src/routes/userRoutes.tsx
+++ fe/src/routes/userRoutes.tsx
@@ -1,7 +1,10 @@
 // userRoutes.js
 
+import PackageDetailPage from "~/pages/user/packages/PackageDetail";
 import RoleBasedRoute from "./roleBasedRoute";
 import DashboardLayout from "~/compoments/layout/DashboardLayout";
+import PackagesPage from "~/pages/user/packages/Packages";
+import ChangePassword from "~/pages/user/profile/ChangePassword";
 import ProfilePage from "~/pages/user/profile/Profile";
 
 const userRoutes = {
@@ -13,7 +16,9 @@ const userRoutes = {
       children: [
         // { path: "dashboard", element: <Dashboard /> },
         { path: "profile", element: <ProfilePage /> },
-       
+        { path: "change-password", element: <ChangePassword /> },
+        { path: "packages", element: <PackagesPage /> },
+         { path: "package-detail/:id", element: <PackageDetailPage /> },
       ],
     },
   ],
