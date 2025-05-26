dcebbecb4996a1f68e3413cb5299701edb2cfd32
Danny2611 <21130584@st.hcmuaf.edu.vn>
2025-05-27 00:43:16 +0700

    PersonalSchedule for workout & appoinemt,  service & ui for payment

--- fe/src/routes/userRoutes.tsx
+++ fe/src/routes/userRoutes.tsx
@@ -6,6 +6,15 @@ import DashboardLayout from "~/compoments/layout/DashboardLayout";
 import PackagesPage from "~/pages/user/packages/Packages";
 import ChangePassword from "~/pages/user/profile/ChangePassword";
 import ProfilePage from "~/pages/user/profile/Profile";
+import PackageRegistrationPage from "~/pages/user/packages/PackageRegistrationPage";
+import MyPackagesPage from "~/pages/user/memberships/Memberships";
+import PaymentStatusPage from "~/pages/user/payments/PaymentStatusPage";
+import TrainerList from "~/pages/user/trainner/TrainerList";
+import BookingPage from "~/pages/user/appoinments/BookingPage";
+import ManageAppointments from "~/pages/user/appoinments/ManageAppointments";
+import RescheduleAppointmentPage from "~/pages/user/appoinments/RescheduleAppointmentPage";
+import PersonalSchedulePage from "~/pages/user/schedules/PersonalSchedulePage";
+import CreateWorkoutPage from "~/pages/user/workoutSchedule/CreateWorkoutPage";
 
 const userRoutes = {
   path: "user",
@@ -18,7 +27,30 @@ const userRoutes = {
         { path: "profile", element: <ProfilePage /> },
         { path: "change-password", element: <ChangePassword /> },
         { path: "packages", element: <PackagesPage /> },
-         { path: "package-detail/:id", element: <PackageDetailPage /> },
+        { path: "package-detail/:id", element: <PackageDetailPage /> },
+        { path: "packages-register/:id", element: <PackageRegistrationPage /> },
+         { path: "my-packages", element: <MyPackagesPage /> },
+
+        {
+          path: "payment",
+          children: [
+            { path: "success", element: <PaymentStatusPage /> },
+            { path: "failed", element: <PaymentStatusPage /> },
+          ],
+        },
+
+         { path: "list-trainer", element: <TrainerList /> },
+          { path: "book-appointment/:trainerId", element: <BookingPage /> },
+         { path: "manage-appointment", element: <ManageAppointments /> },
+
+          {
+          path: "reschedule-appointment/:appointmentId",
+          element: <RescheduleAppointmentPage />,
+        },
+         // schedule
+        { path: "my-schedule", element: <PersonalSchedulePage /> },
+          //workoutSchedule:
+        { path: "workout", element: <CreateWorkoutPage /> },
       ],
     },
   ],
